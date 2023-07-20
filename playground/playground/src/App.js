878a8cee31f9d65d94096ee4d2846c45e23850ba
DJGofficial <davidjgoebels@gmail.com>
2023-07-20 20:07:12 +0200

    Conditional rendering

--- playground/playground/src/App.js
+++ playground/playground/src/App.js
@@ -1,25 +1,38 @@
 import { useState } from 'react'
 
+const History = (props) => {  
+  if (props.allClicks.length === 0) {    
+    return (      
+    <div>
+      the app is used by pressing the buttons      
+      </div>
+          )  
+          }  
+          return (    
+          <div>      
+           button press history: {props.allClicks.join(' ')}    
+           </div>  
+           )
+          }
+
+          const Button = ({ handleClick, text }) => ( 
+             <button onClick={handleClick}>    
+             {text}  
+             </button>)
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right) 
+    setLeft(left + 1)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    console.log('right before', right)
     setRight(right + 1)
-    console.log('right before', right)
-
-    setTotal(left + right)
   }
 
   return (
@@ -28,8 +41,7 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>
-      <p>total {total}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
