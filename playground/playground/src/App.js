23d23bb34deeb256ea936a6af0a93573629c99bc
DJGofficial <120133080+DJGofficial@users.noreply.github.com>
2023-07-20 13:44:34 +0200

    Handling arrays

--- playground/playground/src/App.js
+++ playground/playground/src/App.js
@@ -1,4 +1,22 @@
 import { useState } from 'react'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {    setAll(allClicks.concat('L'))    setLeft(left + 1)  }
+  const handleRightClick = () => {    setAll(allClicks.concat('R'))    setRight(right + 1)  }
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>    </div>
+  )
+}
+export default App;
+/*
 const App = () => {
   const [clicks, setClicks] = useState({
     left: 0, right: 0
@@ -20,7 +38,7 @@ const handleRightClick = () =>
     </div>
   )
 }
-export default App;
+
 /*
 const Display = ({ counter }) => <div>{counter}</div>
 
