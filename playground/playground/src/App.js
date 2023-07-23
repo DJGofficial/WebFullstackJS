31dc2c1d793b485961e0fc238acb805b083c353a
DJGofficial <davidjgoebels@gmail.com>
2023-07-23 09:51:11 +0200

    A function that returns a function Part2

--- playground/playground/src/App.js
+++ playground/playground/src/App.js
@@ -3,15 +3,17 @@ import { useState } from 'react'
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const hello = () => {    
-  const handler = () => console.log('hello world')    
-  return handler  }
-
+  const setToValue = (newValue) => {    
+    console.log('value now', newValue)    
+  setValue(newValue)  }
+  
   return (
     <div>
       {value}
-    <button onClick={hello()}>button</button>
-    </div>
+     <button onClick={()=>setToValue(1000)}>thousand</button>      
+     <button onClick={()=>setToValue(0)}>reset</button>      
+     <button onClick={()=>setToValue(value + 1)}>increment</button>
+      </div>
   )
 }
 
