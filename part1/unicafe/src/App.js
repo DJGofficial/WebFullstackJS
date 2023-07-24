8a20bcdf1732b6a784e421e16876de2461042a26
DJGofficial <davidjgoebels@gmail.com>
2023-07-24 12:40:08 +0200

    1.8 and 1.9 unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,24 +1,52 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+ if(props.all>0){
+    return (
+      <div>
+      <div>good {props.good}</div>
+      <div>neutral {props.neutral}</div>
+      <div>bad {props.bad}</div>
+      <div>all {props.all}</div>
+      <div>average {props.average}</div>
+      <div>positive {props.positive} %</div>
+      </div>
+    )
+  }else{
+    return <div>No feedback given</div>;
+  }
+}
+
 const App = () => {
   // save clicks of each button to its own state
+ 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll]= useState(0)
   const average =((good * 1) + (bad * -1))/all
   const positive= good*100/all
+  const [allStats, setStats] = useState([])
 
-  const handleGood = (props)=>()=>{
+  const props = {
+    good,
+    neutral,
+    bad,
+    all,
+    average,
+    positive,
+  };
+
+  const handleGood = ()=>()=>{
   setGood(good+1)
   setAll(all+1)
 }
-const handleNeutral = (props)=>()=>{
+const handleNeutral = ()=>()=>{
   setNeutral(neutral+1)
   setAll(all+1)
 
 }
-const handleBad = (props)=>()=>{
+const handleBad = ()=>()=>{
   setBad(bad+1)
   setAll(all+1)
 }
@@ -35,12 +63,8 @@ const handleBad = (props)=>()=>{
         bad
         </button>
         <h1>statistics</h1>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {all}</div>
-        <div>average {average}</div>
-        <div>positive {positive} %</div>
+    
+        <Statistics {...props}/>
     </div>
   )
 }
